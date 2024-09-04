0694c4aaf66e61d9c95e76229d4f40d9e6125732
Trang Mai Xuan <trang.maixuan@phenikaa-uni.edu.vn>
2024-09-04 20:15:26 +0700

    Update github link

--- src/data/CourseStoreData.ts
+++ src/data/CourseStoreData.ts
@@ -4,7 +4,7 @@ export const courseStoreData: CourseStoreData = {
   // Link to course Canvas. No trailing slash.
   linkCanvas: "https://canvas.phenikaa-uni.edu.vn/courses/11842",
   // Link to course GitHub.
-  linkGitHub: "https://github.com/tbd",
+  linkGitHub: "https://github.com/PhenikaaCS/web-cse703016-24s1",
   // Link to university syllabus policies and guidelines.
   linkUniversitySyllabusGuidelines:
     "",
